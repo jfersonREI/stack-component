49f7b97c8831545d2eef0a664c0955c10cabbbd8
jfersonREI <jeff.ferson@reisystems.com>
2025-05-15 21:34:47 -0400

    gh-page

--- stack-component/src/main.jsx
+++ stack-component/src/main.jsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={"/stack-component"}>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/responsive" element={<ResponsiveDemo />} />
